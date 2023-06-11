ed64ed93951727ebd51881b491c65eca6abdb49d
Marco Sérvio <marcoservio22@hotmail.com>
2023-06-11 15:34:06 -0300

    Projeto inicial

---
+++ CatalogoCarros/Program.cs
@@ -0,0 +1,43 @@
+using Prometheus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var counter = Metrics.CreateCounter("web_api_catalogo_carro", "Conta quantas requisições cada endpont recebe.",
+    new CounterConfiguration
+    {
+        LabelNames = new[] { "method", "endpoint" }
+    });
+
+app.Use((context, next) =>
+{
+    counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+    return next();
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseMetricServer();
+
+app.UseHttpMetrics();
+
+app.Run();
---
+++ client/client.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HOST='app-catalogo-api'
+
+while true
+    do
+	# ENDP=`expr $RANDOM % 3 + 1`
+	# NUMB=`expr $RANDOM % 100 + 1`
+	# #TEMP=`expr 1 + $(awk -v seed="$RANDOM" 'BEGIN { srand(seed); printf("%.4f\n", rand()) }')`
+        
+	# if [ $NUMB -le 55 ]; then
+	#     curl --silent --output /dev/null http://${HOST}/topicos
+    #     elif [ $NUMB -ge 56 ] && [ $NUMB -le 85 ] ; then
+	#     curl --silent --output /dev/null http://${HOST}/topicos/$ENDP
+    #     elif [ $NUMB -ge 86 ] && [ $NUMB -le 95 ] ; then
+	#     curl --silent --output /dev/null --data '{"email":"moderador@email.com","senha":"123456"}' \
+	# 	 --header "Content-Type:application/json" \
+	# 	 --request POST http://${HOST}/auth
+    #     elif [ $NUMB -ge 96 ] && [ $NUMB -le 98 ] ; then
+	#     curl --silent --output /dev/null --data '{"email":"moderador@email.com","senha":"1234567"}' \
+	#          --header "Content-Type:application/json" \
+	#          --request POST http://${HOST}/auth
+	# else
+	#     curl --silent --output /dev/null http://${HOST}/topicos/0
+    #     fi
+
+	curl --silent --output /dev/null http://${HOST}/WeatherForecast
+
+	#sleep $TEMP
+	sleep 0.75
+done
+
